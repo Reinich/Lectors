819c6be41a30612abc2d2a49b67ab0f5c7a8777d
Reni <reinich@outlook.com>
2022-11-30 19:53:42 +0300

    Add files

---
+++ Lecture 1/Examples/Example005_HelloFavorite/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лола")
+{
+Console.WriteLine("Привет, моя хорошая!");
+}
+else 
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
