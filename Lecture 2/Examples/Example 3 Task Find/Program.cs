3a0b297803357a5984ac412b693a7659acf0d276
Reni <reinich@outlook.com>
2022-11-30 21:04:34 +0300

    Add Lecture 2

---
+++ Lecture 2/Examples/Example 3 Task Find/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int[] array = { 11, 27, 31, 64, 35, 76, 27, 18, 95 };
+int n = array.Length;
+int find = 27;
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
