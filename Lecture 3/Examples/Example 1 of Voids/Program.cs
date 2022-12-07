ebbb3d851e3143b860bd472de1f75ad5e7888a58
Reni <reinich@outlook.com>
2022-12-07 22:20:14 +0300

    Add rename files 2

---
+++ Lecture 3/Examples/Example 1 of Voids/Program.cs
@@ -0,0 +1,61 @@
+﻿void Method1()                           // Первый метод, ничего не принимает, ничего не возвращает. Просто выполняет некоторые действия
+{
+    Console.WriteLine("Автор Ирина");
+}
+Method1();
+
+
+
+
+
+
+void Method2(string msg)                 // Второй метод, принимает значение, но не возвращает его. Просто выполняет действия
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+
+
+
+
+void Method21(string msg, int count)          // Тот же второй метод, вид сбоку
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg: "Текст сообщения", count: 4); // Явно указывается какие переменные, которое должны быть переданы
+
+
+
+
+
+int Method3()                               // Третий метод, возвращает переменные
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+Console.WriteLine(year);
+
+
+
+
+
+
+string Method4(int count, string text) // Четвертый метод, принимает и возвращает переменные (аргументы)
+{                                      // Выделяем переменную, ПКМ, ринейм симбол = меняется везде эта переменная на новый текст
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");  // 10 - это count, "asdf" - это text. Можно менять их местами
+Console.WriteLine(res);
---
+++ Lecture 3/Examples/Example 5 Sort since max to min homework/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)                // Вывести массив в одной строчке
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)                                      // Сортировка массива от макс к мин
+{
+
+    for (int i = 0; i < array.Length - 1; i++)                       // Размер, как всегда, -1
+    {
+        int maxPosition = i;   // переменная в которую вкладывается первый (после - второй и тд) элемент для будущего сравнения
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;  // Сравнивание элемента с максимальным (на данный момент), запоминаем индекс
+
+        }
+
+        int temporary = array[i];                            // Пузырек!
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
