9eeb9bf7f93bac0b5f075fb08e61623e0309445d
Reni <reinich@outlook.com>
2022-12-15 14:56:25 +0300

    Add Lecture 4

---
+++ Lecture 4/Example 1 Ex of 2D massive/Program.cs
@@ -0,0 +1,25 @@
+﻿//                         строки, столбцы
+// string[,] table = new string[2, 3];
+
+// String.Empty; - заполнение массива нулями
+
+// table[0 , 0] table[0 , 1] table[0 , 2] table[0 , 3] table[0 , 4]
+// table[1 , 0] table[1 , 1] table[1 , 2] table[1 , 3] table[1 , 4]
+
+
+
+ string[,] table = new string[2, 5];
+ table[1, 2] = "слово";
+ for (int rows = 0; rows < 2; rows++)
+ {
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+ }
+
+//                      [0, 1] в GetLength;
+// int[,] matr = new int[5, 4]; [0, 0]
+
+
+
---
+++ Lecture 4/Example 4 Factorial recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿double Factorial(int n)     // int был
+{
+    if(n ==1) return 1;
+    else return n * Factorial(n -1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"({i}!) = {Factorial(i)}");
+    // Console.WriteLine(Factorial(i));
+}
+// Console.WriteLine(Factorial(4));
